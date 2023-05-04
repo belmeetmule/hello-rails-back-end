2ac70d1a0e89442148bea3f899536611b063863e
mule <belmeetmule@gmail.com>
2023-05-05 02:01:23 +0300

    configure Rack-cors to resolve CORS issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,10 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://localhost:3001' # Update with your frontend URL
+      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+  end
